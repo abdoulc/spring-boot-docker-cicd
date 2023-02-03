3fcf31dc001f3725301bc62994ddaa22fa196882
abdoulc <abdoulrazak1996@gmail.com>
2023-02-03 16:19:32 +0100

    config h2

--- src/main/java/com/devdoul/com/blog/entity/User.java
+++ src/main/java/com/devdoul/com/blog/entity/User.java
@@ -1,14 +1,12 @@
 package com.devdoul.com.blog.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.Date;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
