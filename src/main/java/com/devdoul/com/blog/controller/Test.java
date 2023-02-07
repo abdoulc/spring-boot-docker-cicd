9da12a5e98bced68e66eb64eace5e894de248055
abdoulc <abdoulrazak1996@gmail.com>
2023-02-07 14:43:53 +0100

    controller

---
+++ src/main/java/com/devdoul/com/blog/controller/Test.java
@@ -0,0 +1,15 @@
+package com.devdoul.com.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Test {
+
+    @GetMapping("/hello")
+    String test(){
+        return  "Hello World";
+    }
+}
